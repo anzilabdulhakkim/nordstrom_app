c47fe81fe0586f5e3168b4c3f9d03c5b1285c5e3
anzilabdulhakkim <anzilabdulhakkim135@gmail.com>
2024-05-05 02:58:55 +0530

    added the app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import "./App.css";
+import AllRoutes  from "./Routes/AllRoutes";
+
+function App() {
+  return (
+    <div className="App">
+      <ChakraProvider>
+        <AllRoutes />
+      </ChakraProvider>
+    </div>
+  );
+}
+
+export default App;
