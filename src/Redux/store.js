1d2e6227787da1191956f8eb5a78bfbec9f6cafa
anzilabdulhakkim <anzilabdulhakkim135@gmail.com>
2024-05-05 02:57:41 +0530

    added the redux

---
+++ src/Redux/store.js
@@ -0,0 +1,17 @@
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
+import { productReducer } from "./reducer";
+
+const thunkMiddleware = (store) => (next) => (action) => {
+  if (typeof action === "function") {
+    return action(store.dispatch);
+  }
+  next(action);
+};
+
+const middleware = applyMiddleware(thunkMiddleware);
+
+export const store = createStore(productReducer, middleware);
+
+export default store;
+
+console.log(store);
