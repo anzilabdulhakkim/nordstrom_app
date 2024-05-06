0fbcbe3abe46e49f9f152c75ae01efc1ce5dd1bc
anzilabdulhakkim <anzilabdulhakkim135@gmail.com>
2024-05-07 03:07:38 +0530

    modified file

--- src/Components/Container/FirstImageDiv.jsx
+++ src/Components/Container/FirstImageDiv.jsx
@@ -13,10 +13,10 @@ import MiddelImage from "./MiddelImage";
 import ScrollingCurrent from "./ScrollingCurrent";
 
 const imageData = [
-  "https://i.postimg.cc/Gh3hF38D/Screenshot-2022-10-09-152831.png",
-  "https://i.postimg.cc/8z68WxHW/Screenshot-2022-10-09-152906.png",
-  "https://i.postimg.cc/fyLpbPyV/Screenshot-2022-10-09-152952.png",
-  "https://i.postimg.cc/T38N81VL/Screenshot-2022-10-09-153104.png",
+  "https://n.nordstrommedia.com/it/554b0eb3-a151-411a-a1fd-59b2b35dc5b5.jpeg?h=700&w=1608",
+  "https://n.nordstrommedia.com/it/31f1c07d-6aba-49d9-bcdf-5b970f3dce5c.jpeg?h=700&w=1608",
+  "https://n.nordstrommedia.com/it/2fe15a52-4133-4cac-9b71-ebbf9338a946.jpeg?h=700&w=1608",
+  "https://n.nordstrommedia.com/it/c080392c-ed39-42e9-a8f1-964c8a768ada.jpeg?h=700&w=1608",
 ];
 
 function FirstImageDiv() {
